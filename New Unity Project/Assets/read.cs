b4b2fd57200eb47cbf1c53576a374779b8807ebb
rdunk <rdunk@mail.uoguelph.ca>
2016-06-11 23:00:15 -0400

    I can now parse a file and populate with cubes

---
+++ New Unity Project/Assets/read.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;  
+
+public class read : MonoBehaviour {
+	void Start (){
+
+		string file = "/home/rdunk/crawl.txt";
+		if(File.Exists(file)){
+			int y = 0;
+		    var sr = File.OpenText(file);
+		    var line = sr.ReadLine();
+		    while(line != null){
+		    	for(int x = 0;x<line.Length;x++){
+		    		parse(line[x], x, y);
+		    	}
+
+
+
+		        Debug.Log(line); // prints each line of the file
+		        line = sr.ReadLine();
+		        y++;
+		    }  
+		} else {
+		    Debug.Log("Could not Open the file: " + file + " for reading.");
+		    return;
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void parse(char line, int x, int y){
+		if(line=='c'){
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        	cube.transform.position = new Vector3((float)x, 0.5F, (float)y);
+		}
+	}
+}
